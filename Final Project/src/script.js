bbe51c495a97b5097cbd58212a9b091c17ff47e7
Vlad Shevchenko <vlad.shev@gmail.com>
2018-03-07 23:57:12 -0500

    wed night work

--- Final Project/src/script.js
+++ Final Project/src/script.js
@@ -57,18 +57,51 @@ $(document).ready(function(){
 	var address = "";
   var photos ="";
 
-//DISPLAY RECOMMENDED CRAFT BREWERIES
+
+//DISPLAY CRAFT BREWERIES
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position) {
+  showLocation_lat = position.coords.latitude;
+  showLocation_long = position.coords.longitude;
+  console.log(showLocation_lat);
+  console.log(showLocation_long);
+  getBeer();
+}
+
+// $("#one").click(function() {
+//
+//   result = result.sort(function(a, b){
+//   return b.venue.rating - a.venue.rating;
+//   getBeer(); }) // end of result
+// }); // end of click
+
+
+function getBeer() {
   $.ajax({
     type: 'GET',
     dataType: 'jsonp',
     cache: false,
-    url: 'https://api.foursquare.com/v2/venues/explore?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&intent=browse&near=Toronto&query=craft-brewery&categoryId=50327c8591d4c4b30a586d5d&venuePhotos=1',
+    url: "https://api.foursquare.com/v2/venues/explore?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&intent=checkin&ll="+showLocation_lat+","+showLocation_long+"&query=craft-brewery&categoryId=50327c8591d4c4b30a586d5d&venuePhotos=1",
 
     success: function(response){
       console.log(response);
       $("#craftbeer").show();
       var result = response.response.groups[0].items;
       $("#craftbeer").html("");
+
+    //   result_rating = result_distance.sort(function(a, b){
+    // return b.venue.rating - a.venue.rating;
+// });
+    console.log(result);
+
+//when clicked distance result = result_
+//when clicked rating result = result_sorted
+
 
 // append data for each venue
 				$.each( result, function() {
@@ -88,12 +121,6 @@ $(document).ready(function(){
 					if (this.venue.rating) {
 						rating = '<span class="rating">'+this.venue.rating+'</span>';
 					}
-
-
-          var clientId = "UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE";
-          var clientSecret= "IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL";
-          var url = "https://api.foursquare.com/v2/venues/explore?near=";
-          var prefix = this.venue.photos.groups[0].items[0].prefix;
           var suffix = this.venue.photos.groups[0].items[0].suffix;
           var imgPrefix = "https://igx.4sqi.net/img/general/300x250";
 
@@ -104,78 +131,20 @@ $(document).ready(function(){
         $("#craftbeer").append(appendeddatahtml);
 
 
-        }) //end of each
-      } // end of success
-    }) // end of ajax
 
-    var appendeddatahtml = "";
-  	var phone = "";
-  	var rating = "";
-  	var icon = "";
-  	var address = "";
-    var photos ="";
-    var contact =""
-
-
-    $.ajax({
-      type: 'GET',
-      dataType: 'jsonp',
-      cache: false,
-      url: 'https://api.foursquare.com/v2/venues/search?client_id=UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE&client_secret=IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL&v=20180212&intent=checkin&near=Toronto&query=craft-brewery&categoryId=50327c8591d4c4b30a586d5d',
-
-      success: function(response){
-        console.log(response);
-        $("#distance").show();
-        $("#distance").html("");
-        var distanceResult = response.response.venues;
-
-
-        // append data for each venue
-        				$.each( distanceResult, function() {
-
-        					if (this.contact.formattedPhone) {
-        						phone = "Phone:"+this.contact.formattedPhone;
-        					} else {
-        						phone = "";
-        					}
-
-        					if (this.location.address) {
-        						address = '<p class="subinfo">'+this.location.address+'<br>';
-        					} else {
-        						address = "";
-        					}
-
-        					if (this.rating) {
-        						rating = '<span class="rating">'+this.rating+'</span>';
-        					}
-
-
-                  var clientId = "UYXWDL4J1XESVFDVL4IQS4FKZJVLMCOF0SKNRRWRBBSC0LPE";
-                  var clientSecret= "IFYVZGRK3EVI4DF1JEND5ZHC1K15NP5GAZ3NKXPOVCELZQSL";
-                  var url = "https://api.foursquare.com/v2/venues/explore?near=";
-                  // var prefix = this.photos.groups[0].items[0].prefix;
-                  // var suffix = this.photos.groups[0].items[0].suffix;
-                  // var imgPrefix = "https://igx.4sqi.net/img/general/300x250";
+      }); //end of each
+}
+        }) //end of ajax
 
 
 
-                  appendeddatahtml = '<div class="distance element"><h2><span>'+this.name+'<img src="'+icon+'"> '+rating+'</span></h2>'+address+phone+'</p><p><strong>Total Checkins:</strong> '+this.stats.checkinsCount+'</p>'+'</div>';
-                // $("#distance").append(appendeddatahtml);
+} // end of beer
 
-              }) //end of each
-              } // end of success
-              }) // end of ajax
-
-
-      $("#one").click(function() {
-        if ($(this).is(':checked')) {
-          // alert("works");
-          $("#distance").append(appendeddatahtml);
-          $("#craftbeer").hide();
-          $("#distance").show();
-          // $("#distance").html("");
-        }
-        });
+// $("#two").click(function() {
+//           result = result.sort(function(a, b){
+//           return b.venue.rating - a.venue.rating;
+//           getBeer();    }) // end of result
+//         }); // end of click
 
 
 });
